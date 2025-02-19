ea55217c6b2a25adb7d3d40c7aed189106009e22
Oleh Lozovyk <oleh.lozovyk@gmail.com>
2025-02-19 15:18:58 +0000

    Desktop header styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import type { Metadata } from 'next';
 import { League_Spartan } from 'next/font/google';
 import favicon from './favicon.png';
 import './globals.css';
+import s from './layout.module.scss';
 
 const leagueSpartan = League_Spartan({
   subsets: ['latin'],
@@ -24,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={leagueSpartan.className}>{children}</body>
+      <body className={`${leagueSpartan.className} ${s.body}`}>{children}</body>
     </html>
   );
 }
